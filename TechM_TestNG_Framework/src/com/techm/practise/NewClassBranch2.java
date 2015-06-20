0753e52beb6aa6bd4feb1afb7b1d79e13c912621
Khaja <moinuddinbtech@gmail.com>
2015-06-20 06:00:41 -0500

    Added New Class Branch 2

---
+++ TechM_TestNG_Framework/src/com/techm/practise/NewClassBranch2.java
@@ -0,0 +1,12 @@
+package com.techm.practise;
+
+import org.testng.annotations.Test;
+
+public class NewClassBranch2 {
+
+	@Test
+	public void newBranch2Method(){
+		System.out.println("New Branch 2 Method");
+	}
+	
+}
