7ee9e059c4b06731019659335e3ad8793d2930ae
Khaja <moinuddinbtech@gmail.com>
2015-06-20 05:40:24 -0500

    Branch1 commit1

---
+++ TechM_TestNG_Framework/ant/build/src/com/techm/testng/NewClass.java
@@ -0,0 +1,12 @@
+package com.techm.testng;
+
+import org.testng.annotations.Test;
+
+public class NewClass {
+	
+	@Test
+	public void newClass(){
+		System.out.println("This is a new class method");
+	}
+
+}
