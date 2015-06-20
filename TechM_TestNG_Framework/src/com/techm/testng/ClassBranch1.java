2ad8c4c8a42d21e77f5841087915f5724f0de182
Khaja <moinuddinbtech@gmail.com>
2015-06-20 05:55:08 -0500

    Added a new class ClassBranch1

---
+++ TechM_TestNG_Framework/src/com/techm/testng/ClassBranch1.java
@@ -0,0 +1,12 @@
+package com.techm.testng;
+
+import org.testng.annotations.Test;
+
+public class ClassBranch1 {
+	
+	@Test
+	public void branch1Method(){
+		System.out.println("Branch 1 Method");
+	}
+
+}
